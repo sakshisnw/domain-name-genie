4d3e463f844e1b526bfaacb6b28e1d9bbc68ccee
sakshi <sakshi28112@gmail.com>
2025-04-09 10:24:45 +0530

    Added custom icon and centered layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,16 @@ import streamlit as st
 from generator.tagline_generator import generate_tagline
 from generator.name_generator import generate_name
 
-st.set_page_config(page_title="Domain Name Genie")
-st.title("Domain Name Genie")
+# Page config must come BEFORE any UI elements
+st.set_page_config(
+    page_title="Domain Name Genie",
+    page_icon="🧞‍♂️",
+    layout="centered"
+)
+
 
 if st.button("Generate Name and Tagline"):
     name = generate_name()
     tagline = generate_tagline()
     st.subheader(name)
     st.caption(tagline)
-

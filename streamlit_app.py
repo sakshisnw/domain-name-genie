39896f4c1875e203afe8317e8d468569cb2e5928
sakshi <sakshi28112@gmail.com>
2025-04-09 10:12:11 +0530

    Vonnected name generator to Streamlit UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from generator.name_generator import generate_name
 
 st.set_page_config(page_title="Domain Name Genie")
 st.title("Domain Name Genie")
+
+if st.button("Generate Name"):
+    st.subheader(generate_name())

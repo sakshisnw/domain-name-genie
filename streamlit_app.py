89b09b0b4739f44e22c873a01bc8a19b72d5b1b0
sakshi <sakshi28112@gmail.com>
2025-04-09 10:22:13 +0530

    Integrated tagline generator into app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
+from generator.tagline_generator import generate_tagline
 from generator.name_generator import generate_name
 
 st.set_page_config(page_title="Domain Name Genie")
 st.title("Domain Name Genie")
 
-if st.button("Generate Name"):
-    st.subheader(generate_name())
+if st.button("Generate Name and Tagline"):
+    name = generate_name()
+    tagline = generate_tagline()
+    st.subheader(name)
+    st.caption(tagline)
+

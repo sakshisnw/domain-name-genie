bd01762c6aacd636e4ee80ea67310bce84ac7e63
sakshi <sakshi28112@gmail.com>
2025-04-09 10:20:01 +0530

    Created tagline generator module

---
+++ generator/tagline_generator.py
@@ -0,0 +1,12 @@
+import random
+
+taglines = [
+    "Fueling Ideas into Brands.",
+    "Where Vision Meets Identity.",
+    "Crafting the Future of Tech Names.",
+    "Naming. Simplified.",
+    "Ideas that Stick."
+]
+
+def generate_tagline():
+    return random.choice(taglines)
